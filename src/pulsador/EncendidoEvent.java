b14a90a6cf78d8658d9b8071a4ebaaadebc02352
useredsa <emilio.dominguezs@um.es>
2020-12-05 18:49:20 +0100

    Initial State

---
+++ src/pulsador/EncendidoEvent.java
@@ -0,0 +1,22 @@
+package pulsador;
+
+import java.util.EventObject;
+
+public class EncendidoEvent extends EventObject {
+
+	/**
+	 * 
+	 */
+	protected boolean oldEncendido, newEncendido;
+	public EncendidoEvent(Object fuente, boolean anterior, boolean nuevo) {
+		super(fuente);
+		newEncendido=nuevo;
+		oldEncendido=anterior;
+	}
+	public boolean getNewEncendido(){ 
+		return newEncendido;
+	}
+	public boolean getOldEncendido(){ 
+		return oldEncendido;
+	}
+}
---
+++ src/pulsador/IEncendidoListener.java
@@ -0,0 +1,9 @@
+package pulsador;
+
+import java.util.EventListener;
+import java.util.EventObject;
+
+public interface IEncendidoListener extends EventListener {
+
+	public void enteradoCambioEncendido (EventObject e);
+}
